c935e6d55e86dd6ab226f8b9f34bb148fca8d309
yitinglei <ytlei@umd.edu>
2017-11-21 22:55:02 -0500

    fix TurtleOperator.hpp

--- src/moveTurtle.cpp
+++ src/moveTurtle.cpp
@@ -1,3 +1,40 @@
+/********************************************************************
+ *   MIT License
+ *  
+ *   Copyright (c) 2017 Yi-ting Lei
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ ********************************************************************/
+
+
+/** @file moveTurtle.cpp
+ *  @brief main program for moving turtle
+ *
+ *  A simple node to control Turtlebot motion. This program
+ *  will make turtlebot move forward until it is 0.6 meters from an obstacle in
+ *  front. It will then rotate 90 degrees and look to see if its clear of obstacles. 
+ *  If not, it will continue to rotate until finds a clear path.
+ *
+ *  @author Yi-ting Lei
+ *  @date   11/21/2017
+*/
+
 #include <stdlib.h>
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
